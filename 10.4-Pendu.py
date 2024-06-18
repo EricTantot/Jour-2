96e4f76e7f489cf35f01018dfe585c2aa9c7690e
EricTantot <eric.tantot08@gmail.com>
2024-06-18 15:05:37 +0200

    final

--- 10.4-Pendu.py
+++ 10.4-Pendu.py
@@ -1,14 +1,28 @@
 motMystere = input("Entrer un mot mystère de 6 à 10 lettres: ")
 lettres = []
 
+place = []
+lenght = len(motMystere)
+x = 0
+
+while x < lenght :
+    x = x + 1
+    place.append(x)
+
 for lettre in motMystere:
     lettres.append(lettre)
 
+final = list(zip(lettres,place))
 n = len(lettres)
 found = 0
 total = 0 
 win = 0
 trouvé = []
+
+def trouver_numéro(a, final):
+    for i in final:
+        if i[0] == a:
+            return i[1]
 
 if n < 6:
     print("Veuillez entrer un mot de 6 à 10 lettres.")
@@ -36,7 +50,8 @@ else:
                         win = win + 1
                         break
                     else:
-                        print(f"Vous avez trouvé une lettre: {test}, il vous en reste {n-found} à trouver!")
+                        a = trouver_numéro(test, final)
+                        print(f"Vous avez trouvé une lettre: {test}, elle est à la {a}e place, il vous en reste {n-found} à trouver!")
                 else: 
                     total = total + 1
                     trouvé.append(test)
