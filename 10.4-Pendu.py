912da183f9f7e498b571f24c5cfbf69f8daa0eb2
EricTantot <eric.tantot08@gmail.com>
2024-06-18 14:00:33 +0200

    pas bon

--- 10.4-Pendu.py
+++ 10.4-Pendu.py
@@ -5,8 +5,8 @@ for lettre in motMystere:
     mots.append(lettre)
 
 n = len(mots)
+x = len(mots)
 bon = 0
-test = 0
 
 if n < 6:
     print("Veuillez entrer un mot de 6 à 10 lettres.")
@@ -14,14 +14,18 @@ elif n > 10:
     print("Veuillez entrer un mot de 6 à 10 lettres.") 
 else:
     print(f"Vous avez {n} essais pour deviner le mot mystère: {'_'*n}")
-    while bon < n:
+    while x == 0:
         essai = input("Entrer une lettre: ")
         if essai in mots :
             print(f"Une lettre trouvé: {essai}")
             bon = bon + 1
+            if bon == n:
+                print(f"Vous avez trouvé le mot! {motMystere}")
+                break
         else: 
-            test = test + 1
-            print(f"Essayez encore! Il vous reste {n-test} essais.")
+            x = x - 1
+            print(f"Essayez encore! Il vous reste {x} essais.")
 
-    print(f"Perdu! Le mot était {motMystere}!")
+print(x)
+    
     
