76d64310f0ab53020242e3e97f5c99c26570cea8
EricTantot <eric.tantot08@gmail.com>
2024-06-18 14:22:48 +0200

    Fonctionel

--- 10.4-Pendu.py
+++ 10.4-Pendu.py
@@ -1,31 +1,41 @@
 motMystere = input("Entrer un mot mystère de 6 à 10 lettres: ")
-mots = []
+lettres = []
 
 for lettre in motMystere:
-    mots.append(lettre)
+    lettres.append(lettre)
 
-n = len(mots)
-x = len(mots)
-bon = 0
+n = len(lettres)
+found = 0
+total = 0 
+win = 0
+trouvé = []
 
 if n < 6:
     print("Veuillez entrer un mot de 6 à 10 lettres.")
 elif n > 10:
     print("Veuillez entrer un mot de 6 à 10 lettres.") 
 else:
-    print(f"Vous avez {n} essais pour deviner le mot mystère: {'_'*n}")
-    while x == 0:
-        essai = input("Entrer une lettre: ")
-        if essai in mots :
-            print(f"Une lettre trouvé: {essai}")
-            bon = bon + 1
-            if bon == n:
-                print(f"Vous avez trouvé le mot! {motMystere}")
-                break
+    print(f"Vous avez {n} essais pour trouver le mot mystère → {'_'*n}")
+    while total < n :
+        test = input("Entrer une lettre: ")
+        if test in trouvé:
+            print(f"Vous avez déjà proposé cette lettre: {test}")
         else: 
-            x = x - 1
-            print(f"Essayez encore! Il vous reste {x} essais.")
+            if test in lettres:
+                trouvé.append(test)
+                found = found + 1
+                if found == n :
+                    win = win + 1
+                    break
+                else:
+                    print(f"Vous avez trouvé une lettre: {test}\nIl vous en reste {n-found} à trouver!")
+            else: 
+                total = total + 1
+                trouvé.append(test)
+                print(f"Mauvaise lettre! Il vous reste {n-total} essais.")
 
-print(x)
-    
-    
+    if win == 0:
+        print(f"Perdu! Le mot était {motMystere}!")
+    else:
+        print(f"Vous avez trouvé le mot {motMystere}. Bien joué!")
+        
